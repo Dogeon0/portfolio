6040a1149a5bae7ad5cd4816531db810e3d2c943
Dogeon0 <sandout9@gmail.com>
2026-01-22 20:03:02 -0300

    minor changes

--- code.js
+++ code.js
@@ -3,3 +3,24 @@ const swup = new Swup({
     new SwupSlideTheme()
   ]
 });
+
+
+function initResponsiveFooter() {
+    const footer = document.querySelector('footer');
+    if (!footer) return;
+
+    const applyFooterPosition = () => {
+      if (window.innerHeight > 700) {
+        footer.classList.add('position-fixed', 'bottom-0', 'start-0', 'w-100');
+        footer.classList.remove('position-static');
+      } else {
+        footer.classList.remove('position-fixed', 'bottom-0', 'start-0', 'w-100');
+        footer.classList.add('position-static');
+      }
+    };
+
+    applyFooterPosition();
+    window.addEventListener('resize', applyFooterPosition);
+  }
+
+  document.addEventListener('DOMContentLoaded', initResponsiveFooter);
